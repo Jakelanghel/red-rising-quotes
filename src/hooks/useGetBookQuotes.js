6d199fd3238b40350c575f20d4371f838f390251
Jake <jacobtlanghel@gmail.com>
2023-07-12 22:21:10 -0400

    refactor hooks

--- src/hooks/useGetBookQuotes.js
+++ src/hooks/useGetBookQuotes.js
@@ -5,31 +5,30 @@ import { filterName } from "../util/filterName";
 export const useGetBookQuotes = () => {
   const [quotesData, setQuotesData] = useState(null);
 
-  const getBookQuotes = (selectedBook, length) => {
-    let url;
-    let quotesKey;
+  const getBookQuotes = async (selectedBook, length) => {
+    try {
+      let url;
+      let quotesKey;
 
-    // if selectedBook is All fetch all quotes
-    if (selectedBook !== "all-books") {
-      url = `http://localhost:5000/api/red-rising/game/?slug=${selectedBook}`;
-      quotesKey = "quotes";
-    } else {
-      url = "http://localhost:5000/api/red-rising/all/";
-      quotesKey = "results";
+      // if selectedBook is All fetch all quotes
+      if (selectedBook !== "all-books") {
+        url = `http://localhost:5000/api/red-rising/game/?slug=${selectedBook}`;
+        quotesKey = "quotes";
+      } else {
+        url = "http://localhost:5000/api/red-rising/all/";
+        quotesKey = "results";
+      }
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      // filter out quotes that are too long
+      let modifiedQuotes = filterQuotes(data[quotesKey], length);
+      modifiedQuotes = filterName(modifiedQuotes);
+      setQuotesData({ ...data, quotes: modifiedQuotes });
+    } catch (error) {
+      console.log(error);
     }
-
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        // filter out quotes that are to long
-        console.log(data[quotesKey]);
-        let modifiedQuotes = filterQuotes(data[quotesKey], length);
-        modifiedQuotes = filterName(modifiedQuotes);
-        setQuotesData({ ...data, quotes: modifiedQuotes });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
   };
 
   return { quotesData, getBookQuotes };
